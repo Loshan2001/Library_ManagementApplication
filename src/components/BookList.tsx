fde646247e22d7cf0e77728fca5693385b87930e
loshan <it21803970@my.sliit.lk>
2024-08-28 17:15:57 +0530

    implemet book list page

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,157 +1,40 @@
-import React from 'react'
-import './Home/Home.css'
-const BookList:React.FC=()=> {
+
+
+import React from 'react';
+import './Home/Home.css';
+import Dummy from './helper/Dummydata';
+
+const BookList: React.FC = () => {
   return (
-    <>
-      <div className='bookContainer shadow-2x'>
-        <div className='flex flex-col w-48 flex-wrap bg-white p-2 outline-slate-500 outline-offset-0 '>
-          <div className='flex justify-around'>
-          <h1 className='font-semibold capitalize'>Title</h1>
-         
+    <div className='bookContainer'>
+      <div className='bookList'>
+        {Dummy.map((book, index) => (
+          <div key={index} className='bookItem'>
+            <div className='bookTitle'>
+              <h1 className='font-semibold capitalize'>{book.title}</h1>
+            </div>
+            <div className='bookImage'>
+              <img
+                src={book.image}
+                alt={book.title}
+                className='w-full h-auto'
+              />
+            </div>
+            <div className='bookDetails'>
+              <p>{book.description}</p>
+              <h3 className='text-slate-500'>- {book.author}</h3>
+              <button className='bg-secondary text-black hover:bg-black hover:text-white uppercase'>
+                Update
+              </button>
+              <button className='hover:bg-black hover:text-white bg-primary uppercase'>
+                Delete
+              </button>
+            </div>
           </div>
-          <div>
-          <img src="https://images.pexels.com/photos/1643033/pexels-photo-1643033.jpeg?cs=srgb&dl=pexels-natalie-bond-320378-1643033.jpg&fm=jpg" alt="book" 
-          className=''
-          />
-          </div>
-          <div className='flex flex-col justify-center text-center p-0 gap-2'>
-            
-            <p>description</p>
-            <h3 className='self-end text-slate-500'>- Author</h3>
-            <button className='bg-secondary text-black hover:bg-black hover:text-white uppercase'>Update</button>
-            <button className='hover:bg-black hover:text-white bg-primary uppercase'>Delete</button>
-          </div>
-        </div>
-
-        <div className='flex flex-col w-48 flex-wrap bg-white p-2 outline-slate-500 outline-offset-0 '>
-          <div className='flex justify-around'>
-          <h1 className='font-semibold capitalize'>Title</h1>
-         
-          </div>
-          <div>
-          <img src="https://images.pexels.com/photos/1643033/pexels-photo-1643033.jpeg?cs=srgb&dl=pexels-natalie-bond-320378-1643033.jpg&fm=jpg" alt="book" 
-          className=''
-          />
-          </div>
-          <div className='flex flex-col justify-center text-center p-0 gap-2'>
-            
-            <p>description</p>
-            <h3 className='self-end text-slate-500'>- Author</h3>
-            <button className='bg-secondary text-black hover:bg-black hover:text-white uppercase'>Update</button>
-            <button className='hover:bg-black hover:text-white bg-primary uppercase'>Delete</button>
-          </div>
-        </div>
-        <div className='flex flex-col w-48 flex-wrap bg-white p-2 outline-slate-500 outline-offset-0 '>
-          <div className='flex justify-around'>
-          <h1 className='font-semibold capitalize'>Title</h1>
-         
-          </div>
-          <div>
-          <img src="https://images.pexels.com/photos/1643033/pexels-photo-1643033.jpeg?cs=srgb&dl=pexels-natalie-bond-320378-1643033.jpg&fm=jpg" alt="book" 
-          className=''
-          />
-          </div>
-          <div className='flex flex-col justify-center text-center p-0 gap-2'>
-            
-            <p>description</p>
-            <h3 className='self-end text-slate-500'>- Author</h3>
-            <button className='bg-secondary text-black hover:bg-black hover:text-white uppercase'>Update</button>
-            <button className='hover:bg-black hover:text-white bg-primary uppercase'>Delete</button>
-          </div>
-        </div>
-        <div className='flex flex-col w-48 flex-wrap bg-white p-2 outline-slate-500 outline-offset-0 '>
-          <div className='flex justify-around'>
-          <h1 className='font-semibold capitalize'>Title</h1>
-         
-          </div>
-          <div>
-          <img src="https://images.pexels.com/photos/1643033/pexels-photo-1643033.jpeg?cs=srgb&dl=pexels-natalie-bond-320378-1643033.jpg&fm=jpg" alt="book" 
-          className=''
-          />
-          </div>
-          <div className='flex flex-col justify-center text-center p-0 gap-2'>
-            
-            <p>description</p>
-            <h3 className='self-end text-slate-500'>- Author</h3>
-            <button className='bg-secondary text-black hover:bg-black hover:text-white uppercase'>Update</button>
-            <button className='hover:bg-black hover:text-white bg-primary uppercase'>Delete</button>
-          </div>
-        </div>
-        <div className='flex flex-col w-48 flex-wrap bg-white p-2 outline-slate-500 outline-offset-0 '>
-          <div className='flex justify-around'>
-          <h1 className='font-semibold capitalize'>Title</h1>
-         
-          </div>
-          <div>
-          <img src="https://images.pexels.com/photos/1643033/pexels-photo-1643033.jpeg?cs=srgb&dl=pexels-natalie-bond-320378-1643033.jpg&fm=jpg" alt="book" 
-          className=''
-          />
-          </div>
-          <div className='flex flex-col justify-center text-center p-0 gap-2'>
-            
-            <p>description</p>
-            <h3 className='self-end text-slate-500'>- Author</h3>
-            <button className='bg-secondary text-black hover:bg-black hover:text-white uppercase'>Update</button>
-            <button className='hover:bg-black hover:text-white bg-primary uppercase'>Delete</button>
-          </div>
-        </div>
-        <div className='flex flex-col w-48 flex-wrap bg-white p-2 outline-slate-500 outline-offset-0 '>
-          <div className='flex justify-around'>
-          <h1 className='font-semibold capitalize'>Title</h1>
-         
-          </div>
-          <div>
-          <img src="https://images.pexels.com/photos/1643033/pexels-photo-1643033.jpeg?cs=srgb&dl=pexels-natalie-bond-320378-1643033.jpg&fm=jpg" alt="book" 
-          className=''
-          />
-          </div>
-          <div className='flex flex-col justify-center text-center p-0 gap-2'>
-            
-            <p>description</p>
-            <h3 className='self-end text-slate-500'>- Author</h3>
-            <button className='bg-secondary text-black hover:bg-black hover:text-white uppercase'>Update</button>
-            <button className='hover:bg-black hover:text-white bg-primary uppercase'>Delete</button>
-          </div>
-        </div>
-        <div className='flex flex-col w-48 flex-wrap bg-white p-2 outline-slate-500 outline-offset-0 '>
-          <div className='flex justify-around'>
-          <h1 className='font-semibold capitalize'>Title</h1>
-         
-          </div>
-          <div>
-          <img src="https://images.pexels.com/photos/1643033/pexels-photo-1643033.jpeg?cs=srgb&dl=pexels-natalie-bond-320378-1643033.jpg&fm=jpg" alt="book" 
-          className=''
-          />
-          </div>
-          <div className='flex flex-col justify-center text-center p-0 gap-2'>
-            
-            <p>description</p>
-            <h3 className='self-end text-slate-500'>- Author</h3>
-            <button className='bg-secondary text-black hover:bg-black hover:text-white uppercase'>Update</button>
-            <button className='hover:bg-black hover:text-white bg-primary uppercase'>Delete</button>
-          </div>
-        </div>
-        <div className='flex flex-col w-48 flex-wrap bg-white p-2 outline-slate-500 outline-offset-0 '>
-          <div className='flex justify-around'>
-          <h1 className='font-semibold capitalize'>Title</h1>
-         
-          </div>
-          <div>
-          <img src="https://images.pexels.com/photos/1643033/pexels-photo-1643033.jpeg?cs=srgb&dl=pexels-natalie-bond-320378-1643033.jpg&fm=jpg" alt="book" 
-          className=''
-          />
-          </div>
-          <div className='flex flex-col justify-center text-center p-0 gap-2'>
-            
-            <p>description</p>
-            <h3 className='self-end text-slate-500'>- Author</h3>
-            <button className='bg-secondary text-black hover:bg-black hover:text-white uppercase'>Update</button>
-            <button className='hover:bg-black hover:text-white bg-primary uppercase'>Delete</button>
-          </div>
-        </div>
+        ))}
       </div>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default BookList
+export default BookList;
--- src/components/helper/Dummydata.tsx
+++ src/components/helper/Dummydata.tsx
@@ -1,5 +1,6 @@
 
 export interface Book {
+    id:number;
     title: string;
     author: string;
     description: string;
@@ -9,40 +10,54 @@ export interface Book {
   
   const bookList: Book[] = [
     {
+      id:1,
       title: "The Enchanted Forest",
       author: "Emma Woodhouse",
       description: "A magical journey through a mystical forest where every tree holds a secret. Perfect for readers who enjoy fantasy and adventure.",
       type: "Fantasy",
-      image: "https://example.com/images/the-enchanted-forest.jpg"
+      image: "https://marketplace.canva.com/EAFaQMYuZbo/1/0/1003w/canva-brown-rusty-mystery-novel-book-cover-hG1QhA7BiBU.jpg"
     },
     {
+      id:2,
       title: "The Silent Ocean",
       author: "John Waters",
       description: "An enthralling mystery set on the high seas, where a detective uncovers hidden truths beneath the calm surface of the ocean.",
       type: "Mystery",
-      image: "https://example.com/images/the-silent-ocean.jpg"
+      image: "https://cdn.vectorstock.com/i/1000v/33/05/comic-book-cover-template-vector-16193305.jpg"
     },
     {
+      id:3,
       title: "Cooking Made Simple",
       author: "Sophia Green",
       description: "A straightforward cookbook with easy-to-follow recipes and tips for busy individuals who love delicious food without the fuss.",
       type: "Fiction",
-      image: "https://example.com/images/cooking-made-simple.jpg"
+      image: "https://i.pinimg.com/originals/26/5d/97/265d97583a147c6f38d8cb9f6f7a7ba5.jpg"
     },
     {
+      id:4,
       title: "The Wonders of Space",
       author: "Michael Roberts",
       description: "An educational and visually stunning book that explores the vastness of space and the wonders it holds, perfect for aspiring young astronomers.",
       type: "Fantacy",
-      image: "https://example.com/images/the-wonders-of-space.jpg"
+      image: "https://i.pinimg.com/736x/ac/2a/93/ac2a931f4f2d886b5e1ab3d5ab498ed3.jpg"
     },
     {
+      id:5,
       title: "Life in the City",
       author: "Laura Williams",
       description: "A captivating collection of short stories about the diverse lives and experiences of people living in a bustling city.",
       type: "ShortStories",
-      image: "https://example.com/images/life-in-the-city.jpg"
-    }
+      image: "https://www.shutterstock.com/create/assets/asset-gateway/template/previews/26966-0.png?width=900&format=webp"
+    },
+    {
+      id:6,
+      title: "Life in the City",
+      author: "Laura Williams",
+      description: "A captivating collection of short stories about the diverse lives and experiences of people living in a bustling city.",
+      type: "ShortStories",
+      image: "https://images.template.net/313496/Untitled-edit-online-1.jpg"
+    },
+    
   ];
   
  
