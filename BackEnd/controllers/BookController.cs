87f502cd612c8dd28da6924b5b1e00837764675b
loshan <it21803970@my.sliit.lk>
2024-08-29 14:00:31 +0530

    implement rest api

--- BackEnd/controllers/BookController.cs
+++ BackEnd/controllers/BookController.cs
@@ -5,7 +5,7 @@ namespace BackEnd.Controllers
 
     using Microsoft.AspNetCore.Mvc;
 
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class BooksController : ControllerBase
     {
@@ -36,5 +36,35 @@ namespace BackEnd.Controllers
 
             return Ok(book); // Return the book with status 200
         }
+
+          // POST: api/books/create
+        [HttpPost("create")]
+        public Book createBook(Book book){
+            _bookContext.Books.Add(book);
+            _bookContext.SaveChanges();
+            return book;
+        }
+
+
+         // PUT: api/books/update/{id}
+        [HttpPut("update/{id}")]
+        public ActionResult<Book> updateBook(int id , Book book){
+           var oldUser =  _bookContext.Books.FirstOrDefault(b => b.Id == id);
+           oldUser.Author = book.Author;
+           oldUser.Description = book.Description;
+           oldUser.Title = book.Title;
+           _bookContext.SaveChanges();
+           return oldUser;
+        }
+
+
+           // DELETE: api/books/delete/{id}
+        [HttpDelete("delete/{id}")]
+        public ActionResult<int> deleteBook(int id){
+            _bookContext.Books.Remove(_bookContext.Books.FirstOrDefault(b => b.Id == id));
+            _bookContext.SaveChanges();
+                return id;
+        }
+
     }
 }
